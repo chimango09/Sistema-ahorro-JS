ce2ec8c3cdc294ab2ffc3d95ec3dff906701a13c
chimango09 <matiasvaldi1818@gmail.com>
2025-09-19 19:51:01 -0300

    Se arreglan funciones por funcion flecha

--- script.js
+++ script.js
@@ -1,71 +1,112 @@
-// let ingreso = parseInt(prompt("Introduzca su ingreso del mes: "))
-// let ahorro = parseInt(prompt("Introduzca el porcentaje que quiere ahorrar: "))
-
-
-// function aplicarDescuento(ingreso,porcentaje){
-//     return ingreso * porcentaje/100
-// }
-
-// resultado = aplicarDescuento(ingreso,ahorro)
-
-// let operacion = prompt("Ingrese la operacion a realizar (suma, resta, multiplicacion, division): ")
-
-// let a = parseInt(prompt("Ingrese el primer numero: "))
-// let b = parseInt(prompt("Ingrese el segundo numero: "))
-
-// function suma(num1,num2){
-//     return num1 + num2
-// }
-
-// function resta(num1,num2){
-//     return num1 + num2
-// }
-
-// function multi(num1,num2){
-//     return num1 * num2
-// }
-
-// function division(num1,num20){
-//     return num1/num2
-// }
-
-// let resultado;
-
-// const resultados = []
-
-// switch (operacion){
-//     case "suma":
-//         resultado = suma(a,b);
-//         console.log("El resultado de la suma es: "+resultado)
-//         resultados.push(suma)
-//         console.log(resultados)
-//         break;
-
-//     case "resta":
-//         resultado = resta(a,b);
-//         console.log("El resultado de la resta es: "+resultado)
-//         break
-
-//     case "multiplicacion":
-//         resultado = multi (a,b);
-//         console.log("El resultado de la multiplicacion es: "+resultado)
-//         break
-
-//     case "division":
-//         resultado = multi (a,b);
-//         console.log("El resultado de la multiplicacion es: "+resultado)
-//         break
-//     default:
-//         console.log("Operacion no valida")
-// }
-
-let a = parseInt(prompt("Ingrese el monto que quiere ahorrar: ")) 
-let b = parseInt(prompt("Ingrese el porcentaje que quiere ahorrar"))
-
-const calculoAhorro = (ingreso, porcentaje) => {
-    return ingreso * porcentaje/100
+function calcularAhorro(dinero,porcentaje){
+    return dinero * (porcentaje/100)
 }
 
-let resultado = calculoAhorro(a,b)
+function suma(n1,n2){
+    return n1+n2
+}
 
-console.log("El monto a ahorrar es de: " + resultado)
+function resta(n1,n2){
+    return n1-n2
+}
+
+function multiplicacion(n1,n2){
+    return n1*n2
+}
+
+function division(n1,n2){
+    if (n2!=0){
+        return n1/n2
+    } else{
+        alert("La divison por cero no esta permitida")
+    }
+}
+
+function guardarIngreso(ingreso){
+    ingresos.push(ingreso)
+}
+
+function ultimoIngreso(){
+    let indice = (ingresos.length - 1)
+    let ultimo_ing = ingresos[indice]
+    alert("Su ultimo ingreso es de: "+ ultimo_ing)
+}
+
+function mostrarTodo(){
+    let mostrarIngresos = ingresos.join("\n - ")
+    alert("Los ingresos guardados son los siguientes: \n - " + mostrarIngresos)
+    console.log(ingresos)
+}
+
+const ingresos = [5000,1000,3000,4000]
+
+let seguir = true
+
+while (seguir){
+    let opcion = parseInt(prompt("Bienvendido/a! \n ¿Que desea hacer? \n 1- Calcular ahorro \n 2- Calculadora \n 3-Consultar ultimo ingreso \n 4-Consultar todos los registros \n 5-Salir "))
+    if (opcion === 5){
+        seguir = false
+    } else {
+        switch (opcion){
+            case 1:
+                let sueldo = parseFloat(prompt("Por favor ingrese su sueldo: "))
+                let ahorro = parseInt(prompt("¿Que porcentaje desea ahorrar?"))
+                let resultado = calcularAhorro(sueldo,ahorro)
+                alert("El ahorro calculado es de: " + resultado)
+                let guardar = prompt("Desea guardar el resultado? (S / N)")
+                if (guardar === "S"){
+                    guardarIngreso()
+                    alert("El resultado a sido guardado correctamente")
+                    
+                }
+                break
+                ;
+            case 2:
+                let opcionCalculadora = parseInt(prompt("¿Que operacion desea realizar? \n 1- Suma \n 2-Resta \n 3-Multiplicacion \n 4-Division"))
+                let numero1 = parseInt(prompt("Ingrese el primer digito: "))
+                let numero2 = parseInt(prompt("Ingrese el segundo digito: "))
+                switch (opcionCalculadora){
+                    case 1:
+                        let resultado_suma = suma(numero1,numero2)  
+                        alert("El resultado de la suma es: "+ resultado_suma)
+                        break
+                        ;
+                    case 2:
+                        let resultado_resta = resta(numero1,numero2)  
+                        alert("El resultado de la resta es: "+ resultado_resta)
+                        break
+                        ;
+                    case 3:
+                        let resultado_multi = multiplicacion(numero1,numero2)  
+                        alert("El resultado de la multiplicacion es: "+ resultado_multi)
+                        break
+                        ;
+                    case 4:
+                        let resultado_div = division(numero1,numero2)  
+                        alert("El resultado de la division es: "+ resultado_div)
+                        break
+                        ;
+                    default:
+                        alert("Seleccione una opcion valida!")    
+                    ;
+                }
+                break
+            case 3:
+                ultimoIngreso()
+                break
+            ;
+            case 4:
+                mostrarTodo()
+                break    
+            ;
+            default:
+                alert("Opcion incorrecta, vuelva a intentarlo")    
+            ;
+
+        }
+    }
+}
+
+
+
+
